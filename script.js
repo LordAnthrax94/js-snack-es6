4aca21ce601b4af4975b5e9e5ad48509e85df21c
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-27 16:39:51 +0100

    fix: aggiornata funzione con math.random

--- script.js
+++ script.js
@@ -86,8 +86,10 @@ const squads = [
 
 
 function valoriArray(max, min){
-  return Math.floor(Math.random(max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
+console.log(squads);
+
 
 squads.forEach(squad => {
   squad.punti = valoriArray(30, 1)
